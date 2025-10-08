c3cbce410f04249d1f35bd0eb832bde85c377479
22kahz <anzoariandrie@email.com>
2025-10-08 17:05:10 +1100

    Initial commit

---
+++ vega_lite_vis.js
@@ -0,0 +1,9 @@
+var vg_1 = "vegalitevis/map.vg.json";
+
+vegaEmbed("#map", vg_1).then(function(result) {
+}).catch(console.error);
+
+var vg_2 = "vegalitevis/linechart.vg.json";
+
+vegaEmbed("#linechart", vg_2).then(function(result) {
+}).catch(console.error);

405c1d8c488d0542df0bb8e6f0599f7b605c0803
22kahz <anzoariandrie@email.com>
2025-10-13 10:44:38 +1100

    Uploaded files and created html draft

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -7,3 +7,28 @@ var vg_2 = "vegalitevis/linechart.vg.json";
 
 vegaEmbed("#linechart", vg_2).then(function(result) {
 }).catch(console.error);
+
+var vg_3 = "vegalitevis/sector.vg.json";
+
+vegaEmbed("#sector", vg_3).then(function(result) {
+}).catch(console.error);
+
+var vg_4 = "vegalitevis/emissiontarget.vg.json";
+
+vegaEmbed("#emissiontarget", vg_4).then(function(result) {
+}).catch(console.error);
+
+var vg_5 = "vegalitevis/temperature.vg.json";
+
+vegaEmbed("#temperature", vg_5).then(function(result) {
+}).catch(console.error);
+
+var vg_6 = "vegalitevis/sector.vg.json";
+
+vegaEmbed("#sector", vg_6).then(function(result) {
+}).catch(console.error);
+
+var vg_7 = "vegalitevis/emissionsbygas.vg.json";
+
+vegaEmbed("#emissionsbygas", vg_7).then(function(result) {
+}).catch(console.error);
